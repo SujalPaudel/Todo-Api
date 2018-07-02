f3693cecd07094ea619820164ea3ee0fc3b5a1b1
Velar <Sujal Paudel>
2018-07-02 19:18:36 +0545

    Implemented typechecking, required, default, trim, minlength, functionality

--- server/server.js
+++ server/server.js
@@ -7,12 +7,26 @@ mongoose.connect('mongodb://localhost/TodoApp');
 var Todo = mongoose.model('Todo', {
   text:{
     type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
     type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
+  }
+});
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
   }
 });
 
@@ -26,14 +40,18 @@ var Todo = mongoose.model('Todo', {
 //   console.log("Unable to save the data", error);
 // });
 
-var newTodo = new Todo({
-  text: 'Code and Drink Tea simultaneously',
-  completed: true,
-  completedAt: 1730
-});
+// var newTodo = new Todo({text: " Try this out"});
 
-newTodo.save().then((saved) => {
-  console.log("The Todo item is saved", saved);
+// newTodo.save().then((saved) => {
+//   console.log("The Todo item is saved", saved);
+// }, (err) => {
+//   console.log("The item cannot be saved\n ", err);
+// });
+
+var newUser = new User({email: '      thesujal17@gmail.com    '});
+
+newUser.save().then((success) => {
+  console.log("Successfully added a user", success);
 }, (err) => {
-  console.log("The item cannot be saved", err);
+  console.log("Sorry the user cannot be added in the database", err);
 });
