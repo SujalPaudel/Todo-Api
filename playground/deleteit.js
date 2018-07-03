522f6c4ed0eac7dc0f0d4a4d46f76341a400cdb9
Velar <Sujal Paudel>
2018-07-03 08:02:53 +0545

    Added POST /todos route and refactored mongoose

--- playground/deleteit.js
+++ playground/deleteit.js
@@ -1,26 +1,44 @@
-class User{
+// class User{
+//   constructor(name){
+//     var Name;
+//     this.Name = name;
+//   }
+//   sayHi(){
+//     return this.Name;
+//   }
+// };
+
+// var obj = new User("Raju");
+// // console.log(obj.sayHi());
+// console.log(obj.sayHi());
+
+
+// class Todo{
+//   constructor(rajesh){
+//     this.rajesh = rajesh;
+//   }
+//   sayHi(){
+//     console.log(this.rajesh);
+//   }
+// }
+
+// let ram = new Todo("Rajesh");
+// ram.sayHi();
+
+
+var one = class{
   constructor(name){
-    var Name;
-    this.Name = name;
+    this.name = name;
   }
   sayHi(){
-    return this.Name;
+    console.log(`Hello ${this.name}`);
   }
 };
 
-var obj = new User("Raju");
-// console.log(obj.sayHi());
-console.log(obj.sayHi());
+let obj = new one("Determination");
+obj.sayHi();
 
+var user = {name : "Aaman Bhandari", age : 20};
+var {name} = user;
+console.log(name);
 
-class Todo{
-  constructor(rajesh){
-    this.rajesh = rajesh;
-  }
-  sayHi(){
-    console.log(this.rajesh);
-  }
-}
-
-let ram = new Todo("Rajesh");
-ram.sayHi();
--- server/server.js
+++ server/server.js
@@ -1,57 +1,32 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/TodoApp');
+var {mongoose} = require('./db/mongoose');
 
-var Todo = mongoose.model('Todo', {
-  text:{
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var {Todo} = require('./models/todo');
+
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text : req.body.text
+  });
+
+  todo.save().then((saved) => {
+    res.send(saved);
+  }, (error) =>{
+    res.status(400).send(error);
+  });
 });
 
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-});
 
-// var newTodo = new Todo({
-//   text : 'Code and Drink Tea'
-// });
 
-// newTodo.save().then((saved) => {
-//   console.log("The data is saved", saved);
-// }, (error) => {
-//   console.log("Unable to save the data", error);
-// });
-
-// var newTodo = new Todo({text: " Try this out"});
-
-// newTodo.save().then((saved) => {
-//   console.log("The Todo item is saved", saved);
-// }, (err) => {
-//   console.log("The item cannot be saved\n ", err);
-// });
-
-var newUser = new User({email: '      thesujal17@gmail.com    '});
-
-newUser.save().then((success) => {
-  console.log("Successfully added a user", success);
-}, (err) => {
-  console.log("Sorry the user cannot be added in the database", err);
+app.listen( 3000, () => {
+  console.log("Started the server on port 3000");
 });
